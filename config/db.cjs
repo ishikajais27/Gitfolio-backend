c672e7f0b10e2175c7e54e71cb11c9567fb125b6
ishikajais27 <ishikajais09876@gmail.com>
2025-07-19 13:15:32 +0530

    fix bugs

--- config/db.cjs
+++ config/db.cjs
@@ -1,36 +1,36 @@
-// const mongoose = require('mongoose')
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI)
-//     console.log('MongoDB Connected...')
-//   } catch (err) {
-//     console.error('Database connection error:', err.message)
-//     process.exit(1)
-//   }
-// }
-
-// module.exports = connectDB
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI
-    if (!uri) {
-      throw new Error('MONGODB_URI is not defined in environment variables')
-    }
-
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000,
-    })
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log('MongoDB Connected...')
   } catch (err) {
     console.error('Database connection error:', err.message)
-    console.log('Attempted connection URI:', process.env.MONGODB_URI) // Debug
     process.exit(1)
   }
 }
 
 module.exports = connectDB
+// const mongoose = require('mongoose')
+
+// const connectDB = async () => {
+//   try {
+//     const uri = process.env.MONGODB_URI
+//     if (!uri) {
+//       throw new Error('MONGODB_URI is not defined in environment variables')
+//     }
+
+//     await mongoose.connect(uri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       serverSelectionTimeoutMS: 5000,
+//     })
+//     console.log('MongoDB Connected...')
+//   } catch (err) {
+//     console.error('Database connection error:', err.message)
+//     console.log('Attempted connection URI:', process.env.MONGODB_URI) // Debug
+//     process.exit(1)
+//   }
+// }
+
+// module.exports = connectDB
